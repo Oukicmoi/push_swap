e67079e1478e6f1aa94578d703661855ff79c92c
Goulven Traimany <gtraiman@bess-f3r1s11.clusters.42paris.fr>
2024-02-11 22:46:05 +0100

    2

--- push_swap/cost_b_to_a.c
+++ push_swap/cost_b_to_a.c
@@ -6,75 +6,69 @@
 /*   By: gtraiman <gtraiman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 19:14:40 by gtraiman          #+#    #+#             */
-/*   Updated: 2024/02/11 22:08:48 by gtraiman         ###   ########.fr       */
+/*   Updated: 2024/02/11 22:37:25 by gtraiman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "push_swap.h"
 
-int	bigcost(t_stack **stack_a, t_stack **stack_b)
-{
-	int		lowst;
-	t_stack	*tmp;
-	int		valowst;
+#include "libft/libft.h"
+#include "push_swap.h"
 
-	valowst = 0;
-	lowst = INT_MAX;
-	tmp = (*stack_b);
-	while (tmp)
-	{
-		if (lowst > ltlcost(stack_a, stack_b, rchfv(stack_a, tmp->value),
-				ft_intindex(stack_b, tmp->value)))
-		{
-			lowst = ltlcost(stack_a, stack_b, rchfv(stack_a, tmp->value),
-					ft_intindex(stack_b, tmp->value));
-			valowst = tmp->value;
-		}
-		tmp = tmp->next;
-	}
-	return (valowst);
+int    bigcost(t_stack **stack_a, t_stack **stack_b)
+{
+    int        lowst;
+    t_stack    *tmp;
+    int        valowst;
+
+    valowst = 0;
+    lowst = INT_MAX;
+    tmp = (*stack_b);
+    while (tmp)
+    {
+        if (lowst > ltlcost(stack_a, stack_b, rchfv(stack_a, tmp->value),
+                ft_intindex(stack_b, tmp->value)))
+        {
+            lowst = ltlcost(stack_a, stack_b, rchfv(stack_a, tmp->value),
+                    ft_intindex(stack_b, tmp->value));
+            valowst = tmp->value;
+        }
+        tmp = tmp->next;
+    }
+    return (valowst);
 }
 
-int	ltlcost(t_stack **stack_a, t_stack **stack_b, int ia, int ib)
+int    ltlcost(t_stack **stack_a, t_stack **stack_b, int ia, int ib)
 {
-	int	cost;
-	int	tb;
-	int	ta;
+    int    cost;
+    int    tb;
+    int    ta;
 
-	cost = 1;
-	tb = (ft_stsize(stack_b) - 1) / 2;
-	ta = (ft_stsize(stack_a) - 1) / 2;
-	cost += ltlcostif(ta, tb, ia, ib);
-	if (ia > ta && ib > tb)
-	{
-		if (ia <= ib)
-			cost += (ft_stsize(stack_a) - ia);
-		else
-			cost += (ft_stsize(stack_b) - ib);
-	}
-	else if (ia > ta)
-		cost += (ft_stsize(stack_a) - ia);
-	else if (ib > tb)
-		cost += (ft_stsize(stack_b) - ib);
-	return (cost);
-}
-
-int	ltlcostif(int ta, int tb, int ia, int ib)
-{
-	int	cost;
-
-	cost = 0;
-	if (ia <= ta && ib <= tb)
-	{
-		if (ia > ib)
-			cost += ia;
-		else
-			cost += ib;
-	}
-	else if (ia <= ta)
-		cost += ia;
-	else if (ib <= tb)
-		cost += ib;
-	return (cost);
+    cost = 1;
+    tb = (ft_stsize(stack_b) - 1) / 2;
+    ta = (ft_stsize(stack_a) - 1) / 2;
+    if (ia <= ta && ib <= tb)
+    {
+        if (ia > ib)
+            cost += ia;
+        else
+            cost += ib;
+    }
+    else if (ia <= ta)
+        cost += ia;
+    else if (ib <= tb)
+        cost += ib;
+    if (ia > ta && ib > tb)
+    {
+        if (ia <= ib)
+            cost += (ft_stsize(stack_a) - ia);
+        else
+            cost += (ft_stsize(stack_b) - ib);
+    }
+    else if (ia > ta)
+        cost += (ft_stsize(stack_a) - ia);
+    else if (ib > tb)
+        cost += (ft_stsize(stack_b) - ib);
+    return (cost);
 }
